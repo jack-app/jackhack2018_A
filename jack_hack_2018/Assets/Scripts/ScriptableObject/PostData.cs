66c7a76e24ff754d876fbd5dd0e9e30ce694f9e1
shoi <shoi.itou@gmail.com>
2018-05-04 09:54:52 +0900

    ScriptableObjectを作った

---
+++ jack_hack_2018/Assets/Scripts/ScriptableObject/PostData.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MyScriptableObject/PostData", fileName = "PostData")]
+public class PostData : ScriptableObject {
+	public string[] DataPath;
+}
