48f68c441559c82021716661a8587497681231ba
Sugi-Chan <sgnmssuioaai@gmail.com>
2018-05-05 12:30:58 +0900

    くじ引き調整

--- jack_hack_2018/Assets/Scripts/Takarakuji.cs
+++ jack_hack_2018/Assets/Scripts/Takarakuji.cs
@@ -1,22 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Takarakuji : MonoBehaviour {
 	public GameObject Atari;
 	public GameObject Hazure;
 	public UserData UD;
-
+    public Text text;
+    public Text Text;
+    public Text texth;
 	void Start(){
+        
 		int r = Random.Range (0, 2);
 		switch (r) {
 		case 0:
 			Atari.SetActive (true);
-			UD.money += 65536;
+                int getpoint = 10 + Random.Range(0, 31415);
+                UD.money += getpoint;
+                text.text = getpoint + "P獲得！";
+                Text.text = "現在 " + UD.money + "P";
 			break;
 		case 1:
 			Hazure.SetActive (true);
-			break;
+                texth.text= "現在 " + UD.money + "P";
+                break;
 		}
+
 	}
 }
