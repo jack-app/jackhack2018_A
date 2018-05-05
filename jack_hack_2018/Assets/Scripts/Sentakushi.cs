a08b98a8d023aba62fd89259ef9188b0f99e27c9
Sugi-Chan <sgnmssuioaai@gmail.com>
2018-05-05 11:12:35 +0900

    MyPage調整

---
+++ jack_hack_2018/Assets/Scripts/Sentakushi.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sentakushi : MonoBehaviour {
+    public Dropdown dropdown;
+    public string s;
+    public void Sentaku_shi()
+    {
+        switch (dropdown.value)
+        {
+            case 0:s = "movie";break;
+            case 1:s = "image";break;
+            case 2:s = "music";break;
+            case 3:s = "URL";break;
+        }
+
+    }
+}
